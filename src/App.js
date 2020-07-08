c5915fd8b48c759b6847038e523a473e5d80283c
sib <zimliant@protonmail.com>
2020-07-08 08:11:24 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+    render(){
+    return(
+      <div className="App">
+        <h1 className="border border-blue-400"> Hello, Workout! </h1>
+        <ul>
+        </ul>
+      </div>
+    );
+  }
+
+}
+
+export default App;
